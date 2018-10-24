87519ab9aee233df6b520876a75fe4696857a7f7
Himank Pathak <himankpathak@gmail.com>
2018-10-24 19:51:19 +0530

    added boiler_plate.php to easily create new pages in future

---
+++ boiler_plate.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<?php include 'components/header.php';?>
+
+		<title>BookMyMovie | BoilerPlate</title>
+	</head>
+	<body>
+		<?php include 'components/navbar.php';?>
+
+		<div class="container">
+
+
+		</div>
+
+	  <div>
+	    <p class="spacer"></p>
+	  </div>
+
+		<?php include 'components/footer.php';?>
+	</body>
+</html>

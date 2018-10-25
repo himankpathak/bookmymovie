7ef326752284737e6bfb01a2f8f2d1c837126bca
Vrashi <vrashrivastava@gmail.com>
2018-10-26 00:03:27 +0530

    added FAQs file

---
+++ faq.php
@@ -0,0 +1,129 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<?php include 'components/header.php';?>
+
+		<title>BookMyMovie | BoilerPlate</title>
+	</head>
+	<body>
+		<?php include 'components/navbar.php';?>
+
+		<div class="container">
+			<!-- Page Heading/Breadcrumbs -->
+			<div class="row">
+      <div class="col-lg-12">
+        <h1 class="page-header">Support
+          <small>FAQs</small>
+        </h1>
+        <ol class="breadcrumb">
+          <li><a href="index.php">Home</a>
+          </li>
+          <li class="active">Support</li>
+        </ol>
+      </div>
+    </div>
+    <!-- /.row -->
+
+    <!-- Content Row -->
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="panel-group" id="accordion">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h4 class="panel-title">
+                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseOne">Registration and Sign In</a>
+              </h4>
+            </div>
+            <div id="collapseOne" class="panel-collapse collapse">
+              <div class="panel-body">
+							<b>How do I register with BookMyShow?</b><br>
+							Click on the 'Register' link on the homepage of bookmyshow.com. Enter your full name, Email id., contact number, password and click on 'Submit' and you are registered with us.<br><br>
+							<b>Is it necessary to register for booking tickets?</b><br>
+							Although registering is not a compulsion, we would suggest you to register on our website, since it would allow you to view your entire booking history and avail the exciting offers.<br><br>
+							<b>Why am I being asked for my Mobile number?</b><br>
+							Well, the simplest reason being that it is for the security of your transaction and also so that the booking confirmation can be sent to you via SMS. What's more, you also get the dope on discounts and offers.<br><br>
+              </div>
+            </div>
+          </div>
+          <!-- /.panel -->
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h4 class="panel-title">
+                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">Ticket Booking</a>
+              </h4>
+            </div>
+            <div id="collapseTwo" class="panel-collapse collapse">
+              <div class="panel-body">
+							<b>When does advance booking open on BookMyShow?</b><br>
+							Commencement of booking depends entirely on cinema planning and the release date of the movie. Whenever the cinema opens the show for advance booking, the show is automatically made available on BookMyShow.<br><br>
+							<b>I booked tickets through the website but did not get the confirmation email / SMS.</b><br>
+							In case your confirmation details are lost, click on the 'Resend Confirmation' link on the website to resend the confirmation e-mail and SMS. Alternately, please call us on 022 6144 5050, 022 3989 5050 ext 1 for assistance.<br><br>
+              </div>
+            </div>
+          </div>
+          <!-- /.panel -->
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h4 class="panel-title">
+                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseThree">Payment</a>
+              </h4>
+            </div>
+            <div id="collapseThree" class="panel-collapse collapse">
+              <div class="panel-body">
+							<b>My booking has been rejected, but my credit card has been charged? What do I do?</b><br>
+							This is a very rare situation, and can occur in case of a network fluctuation at the venue at the time of confirming the transaction. In this case, a reverse request for the charge on your card will be sent by us the same day to the bank.<br><br>
+							<b>The refund has still not been credited in my credit card account</b><br>
+							As per our company policy, we send a reverse request to the bank by the end of the day. Depending on each bank's policy, it can sometimes take up to 15 working days for the reversal to take effect.<br><br>
+              </div>
+            </div>
+          </div>
+          <!-- /.panel -->
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h4 class="panel-title">
+                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseFour">Troubleshootng</a>
+              </h4>
+            </div>
+            <div id="collapseFour" class="panel-collapse collapse">
+              <div class="panel-body">
+                <b>Why is the speed of this website so slow?</b><br>
+								We have optimized the website to offer the best user experience, we request you to once kindly check your connection with your Internet Service Provider.<br><br>
+								<b>Does this site use cookies?</b><br>
+								Cookies help us customize our site to our customers' requirements, and offer a more personalized service.<br><br>
+								<b>What do I do if my booking is not completed?</b><br>
+								We are sorry for this unlikely inconvenience. If your booking process fails during a payment process, we will notify you if any erroneous charges have been incurred and reverse any amount charged within 3 weeks.<br><br>
+              </div>
+            </div>
+          </div>
+          <!-- /.panel -->
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h4 class="panel-title">
+                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseFive">Cupons</a>
+              </h4>
+            </div>
+            <div id="collapseFive" class="panel-collapse collapse">
+              <div class="panel-body">
+							<b>Do i have to pay for the Coupons?</b><br>
+							Some Coupons are Free. We might charge for some Coupons. The Price, if any, will be clearly displayed while buying the Coupon.<br><br>
+							<b>Are Coupons available for All Events on BookMyShow?</b><br>
+							No, Coupons are available only for Movie bookings.<br><br>
+							<b>How long is the Coupon valid?</b><br>
+							The Coupons are valid from the date of the Movie till days specified for the brand campaign. The Start date and Expiry date for the Coupon will be clearly displayed while picking the coupon. You won’t be able to avail the coupon before and after the coupon validity.<br>
+              </div>
+            </div>
+          </div>
+          <!-- /.panel -->
+        </div>
+        <!-- /.panel-group -->
+      </div>
+      <!-- /.col-lg-12 -->
+		</div>
+
+	  <div>
+	    <p class="spacer"></p>
+	  </div>
+
+		<?php include 'components/footer.php';?>
+	</body>
+</html>
